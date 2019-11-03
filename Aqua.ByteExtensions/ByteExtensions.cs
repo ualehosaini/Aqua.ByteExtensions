aef097ed666ef921ad42257f3a2527063967f719
ualehosaini2019 <ualehosaini2019@hotmail.com>
2019-11-03 01:56:05 +0000

    Boolean ToBytes has been added

---
+++ Aqua.ByteExtensions/ByteExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aqua.ByteExtensions
+{
+    public static class ByteExtensions
+    {
+        /// <summary>
+        /// Returns byte array
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static byte[] ToBytes(this bool input)
+        {
+            return BitConverter.GetBytes(input);
+        }
+    }
+}
