ece115f8d87dbe64cebcd6e72bd071f5053de1bc
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-18 22:41:33 +0000

    Comment added to tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -30,6 +30,9 @@ namespace Aqua.ByteExtensions.Tests
             Assert.Equal(expected, input.ToBytes());
         }
 
+        /// <summary>
+        /// Data
+        /// </summary>
         public static IEnumerable<object[]> ToBytesCharData =>
             new List<object[]>
             {
