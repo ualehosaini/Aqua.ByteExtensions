255cdce5482559a2727c5c38a3d9c48b566084d9
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-23 18:35:00 +0000

    Modified the comments of test

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -295,7 +295,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Data
+        /// Sample data for test
         /// </summary>
         public static IEnumerable<object[]> ToBytesLongReversedData =>
             new List<object[]>
