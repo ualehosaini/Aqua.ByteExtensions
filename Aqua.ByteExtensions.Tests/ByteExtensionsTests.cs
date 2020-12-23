43b1d75320ed68d47bb06bf627fc080d49f3e034
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-23 18:32:40 +0000

    Modified the comments of test

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -71,7 +71,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Data
+        /// Sample data for test
         /// </summary>
         public static IEnumerable<object[]> ToBytesShortData =>
             new List<object[]>
