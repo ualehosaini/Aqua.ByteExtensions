e5d0db18416d3cf632a66723402c092274f0f8a9
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-23 18:36:47 +0000

    Modified the comments of test

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -481,7 +481,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Data
+        /// Sample data for test
         /// </summary>
         public static IEnumerable<object[]> RemoveData =>
             new List<object[]>
