53f1dc7e048a963f6be0cd6ef81bf7dbe8966db1
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-28 22:14:05 +0000

    ToBytes_LongReversed_Valid

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -295,7 +295,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Sample data for test ToBytes_ShortReversed_Valid
+        /// Sample data for test ToBytes_LongReversed_Valid
         /// </summary>
         public static IEnumerable<object[]> ToBytesLongReversedData =>
             new List<object[]>
