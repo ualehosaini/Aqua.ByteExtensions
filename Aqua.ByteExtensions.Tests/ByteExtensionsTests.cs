3d4fa9aff71c2a1f17392f24e6db31d63cbee4e7
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-28 22:12:32 +0000

    ToBytes_ULong_Valid

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -181,7 +181,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Sample data for test
+        /// Sample data for test ToBytes_ULong_Valid
         /// </summary>
         public static IEnumerable<object[]> ToBytesUlongData =>
             new List<object[]>
