70a6956692dfecab549041e05fbd63b28face15b
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-12-28 22:14:16 +0000

    ToBytes_FloatReversed_Valid

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -313,7 +313,7 @@ namespace Aqua.ByteExtensions.Tests
         }
 
         /// <summary>
-        /// Sample data for test
+        /// Sample data for test ToBytes_FloatReversed_Valid
         /// </summary>
         public static IEnumerable<object[]> ToBytesFloatReversedData =>
             new List<object[]>
