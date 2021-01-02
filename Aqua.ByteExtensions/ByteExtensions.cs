1d0e148d41b0543ccee898ec94c8e56947731c61
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-02 19:13:16 +0000

    Comments Refactoring

--- Aqua.ByteExtensions/ByteExtensions.cs
+++ Aqua.ByteExtensions/ByteExtensions.cs
@@ -147,7 +147,7 @@ namespace Aqua.ByteExtensions
         }
 
         /// <summary>
-        /// Returns reversed byte array
+        /// Returns reversed byte array from short
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
