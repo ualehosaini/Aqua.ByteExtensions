640e0cfe147a9d68e0d61382907af357b9949e66
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-18 20:15:09 +0000

    Modernized the tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -148,10 +148,7 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(ToBytesUlongData))]
-        public void ToBytes_ULong_Valid(byte[] expected, ulong input)
-        {
-            Assert.Equal(expected, input.ToBytes());
-        }
+        public void ToBytes_ULong_Valid(byte[] expected, ulong input) => Assert.Equal(expected, input.ToBytes());
 
         /// <summary>
         /// Sample data for test - ToBytes_ULong_Valid
