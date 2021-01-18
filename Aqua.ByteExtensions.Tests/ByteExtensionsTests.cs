1f42d8c94c0b8d1970136f42ab395f77721dfa44
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-18 20:15:01 +0000

    Modernized the tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -133,10 +133,7 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(ToBytesUintData))]
-        public void ToBytes_UInt_Valid(byte[] expected, uint input)
-        {
-            Assert.Equal(expected, input.ToBytes());
-        }
+        public void ToBytes_UInt_Valid(byte[] expected, uint input) => Assert.Equal(expected, input.ToBytes());
 
         /// <summary>
         /// Sample data for test - ToBytes_UInt_Valid
