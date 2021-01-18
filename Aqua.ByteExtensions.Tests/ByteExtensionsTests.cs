4c842b9eb094df25c3aa95a97d763df73dab9a5a
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-18 20:15:27 +0000

    Modernized the tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -179,10 +179,7 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(ToBytesCharReversedData))]
-        public void ToBytes_CharReversed_Valid(byte[] expected, char input)
-        {
-            Assert.Equal(expected, input.ToBytesReversed());
-        }
+        public void ToBytes_CharReversed_Valid(byte[] expected, char input) => Assert.Equal(expected, input.ToBytesReversed());
 
         /// <summary>
         /// Sample data for test - ToBytes_CharReversed_Valid
