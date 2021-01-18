1a1b98bd2d8e12fe9479efabbd03a09e2bc77e9d
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-18 20:14:38 +0000

    Modernized the tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -103,8 +103,7 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(ToBytesFloatData))]
-        public void ToBytes_Float_Valid(byte[] expected, float input)
-        => Assert.Equal(expected, input.ToBytes());
+        public void ToBytes_Float_Valid(byte[] expected, float input) => Assert.Equal(expected, input.ToBytes());
 
         /// <summary>
         /// Sample data for test - ToBytes_Float_Valid
