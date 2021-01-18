6e8d4bb69f1f0f9c9b471f5f77a1efabfdd0e381
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-18 20:17:37 +0000

    Modernized the tests

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -416,10 +416,7 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(ToHexStringData))]
-        public void ToHexString_Valid(string expected, byte[] input)
-        {
-            Assert.Equal(expected, input.ToHexString());
-        }
+        public void ToHexString_Valid(string expected, byte[] input) => Assert.Equal(expected, input.ToHexString());
 
         /// <summary>
         /// Sample data for test - ToHexString_Valid
