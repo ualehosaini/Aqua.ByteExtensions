059a90bdf00e4fc8f8cb7e21a95fe37b4efd5186
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-17 23:47:13 +0000

    Refactored the test methods for better readability

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -72,7 +72,9 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(GetToBytesIntData()))]
-        public void ToBytes_Int_Valid(byte[] expected, int input) => Assert.Equal(expected, input.ToBytes());
+        public void ToBytes_Int_Valid(
+            byte[] expected,
+            int input) => Assert.Equal(expected, input.ToBytes());
 
         /// <summary>
         /// Sample data for test - ToBytes_Int_Valid
