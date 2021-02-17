d255c1b8ca655cc2914c83211551b49e1f6de232
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-17 23:49:22 +0000

    Refactored the test methods for better readability

--- Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
+++ Aqua.ByteExtensions.Tests/ByteExtensionsTests.cs
@@ -278,7 +278,8 @@ namespace Aqua.ByteExtensions.Tests
 
         [Theory]
         [MemberData(nameof(GetToBytesUshortReversedData()))]
-        public void ToBytes_UShortReversed_Valid(byte[] expected, float input) => Assert.Equal(expected, input.ToBytesReversed());
+        public void ToBytes_UShortReversed_Valid(byte[] expected,
+                                                 float input) => Assert.Equal(expected, input.ToBytesReversed());
 
         /// <summary>
         /// Sample data for test - ToBytes_UShortReversed_Valid
